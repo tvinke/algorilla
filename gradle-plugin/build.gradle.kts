c1616b318090b3e9e1d25775edc414c7e2750c20
Ted Vinke <tedvinke@gmail.com>
2026-03-11 14:26:39 +0100

    add plugin-publish plugin for Gradle Plugin Portal

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("algorilla.kotlin-library")
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.3.0"
     alias(libs.plugins.kotlin.serialization)
 }
 
@@ -15,12 +16,15 @@ dependencies {
 }
 
 gradlePlugin {
+    website.set("https://github.com/tvinke/algorilla")
+    vcsUrl.set("https://github.com/tvinke/algorilla")
     plugins {
         create("algorilla") {
             id = "com.github.tvinke.algorilla"
             implementationClass = "com.github.tvinke.algorilla.gradle.AlgorillaPlugin"
             displayName = "Algorilla"
             description = "Detects algorithmic complexity anti-patterns in source code"
+            tags.set(listOf("static-analysis", "performance", "complexity", "linter"))
         }
     }
 }
